778ac4b17ac44e32f195fc4e945bb4ae97541a92
johnmark13 <mark.findlater@googlemail.com>
2022-06-01 16:39:51 +0100

    This folder should not be hecked in
    A healthy package.json file will allow it to be recreated and make wrking with the repo mush more lightweight.

--- node_modules/aphrodite/tests/testUtils.js
+++
@@ -1,7 +0,0 @@
-export const getSheetText = (sheet) => {
-    let allRules = '';
-    for (let i = 0; i < sheet.cssRules.length; i ++) {
-        allRules += sheet.cssRules[i].cssText + ' ';
-    }
-    return allRules;
-};
--- node_modules/aphrodite/typings/font-family.d.ts
+++
@@ -1,8 +0,0 @@
-export type FontFamily =
-    | string
-    | {
-        fontFamily: string;
-        src: string;
-        fontStyle?: string;
-        fontWeight?: string | number;
-      };
